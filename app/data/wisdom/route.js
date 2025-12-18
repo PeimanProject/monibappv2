b4fe6556257fb0f36bfd64df1ffe93e79d63a193
morteza <mortezakhoshki@gmail.com>
2025-12-18 10:23:05 +0330

    `Update route.js: replace page and size variables with p and s`

--- app/data/wisdom/route.js
+++ app/data/wisdom/route.js
@@ -2,10 +2,10 @@
 import { API } from "@/core/config/api";
 
 export async function WisdomReq({ page, size }) {
-  const page = page || 0;
-  const size = size || 100;
+  const p = page || 0;
+  const s = size || 100;
 
-  const listReq = await fetch(`${API().core}wisdom?page=${page}&size=${size}`);
+  const listReq = await fetch(`${API().core}wisdom?page=${p}&size=${s}`);
   const list = await listReq.json();
 
 
